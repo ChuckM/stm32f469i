0888937e5c3cf977c8b7a89b8373606434861d26
Chuck McManis <cmcmanis@mcmanis.com>
2016-07-15 23:07:05 -0700

    QSPI: Now with some writing
    Still isn't working reliably and I'm not yet sure what is going
    on vis-a-vis doing 1 line SPI vs 2 line or 4 line. Could be dummy
    cycles could be something else, but I get weird results that way.

--- demos/qspi/qspi.c
+++ demos/qspi/qspi.c
@@ -16,8 +16,6 @@ void qspi_read_id(uint8_t *res);
 
 #define QSPI_BASE_ADDRESS	((uint8_t *)(0x90000000))
 
-/* one 4K sector from the FLASH chip */
-uint8_t sector_data[4096];
 
 /*
  * Quad SPI initialization
@@ -177,25 +175,32 @@ void qspi_read_sector(uint32_t addr, uint8_t *buf, int len);
  *
  * The Quad SPI peripheral starts when you write the
  * address register. 
+ *
+ * From the Data sheet for the flash, fast read needs
+ * 7 dummy cycles at 86Mhz, 8 at 95Mhz. Since we're at
+ * 168Mhz/2 = 84Mhz we're using 7. But if we went to 180/2
+ * we would set it to 8.
  */
 void
 qspi_read_sector(uint32_t addr, uint8_t *buf, int len)
 {
 	uint32_t ccr;
 	uint32_t tmp;
-	int xfer, flen, bcnt;
+	int i, bcnt;
 
 	printf("Read Sector: Initial Status is\n");
 	print_status(QUADSPI_SR);
 
 	/* wait for port to be non busy */
 	while (QUADSPI_SR & QUADSPI_SR_BUSY) ;
+	QUADSPI_CR &= ~QUADSPI_CR_SSHIFT; /* turn off sample shift */
 
 	QUADSPI_DLR = len - 1;
 	ccr = QUADSPI_SET(CCR, FMODE, QUADSPI_CCR_MODE_IREAD);
+	ccr |= QUADSPI_SET(CCR, DCYC, 9);
 	ccr |= QUADSPI_SET(CCR, ABMODE, QUADSPI_CCR_MODE_NONE);
 	ccr |= QUADSPI_SET(CCR, ABSIZE, 0);
-	ccr |= QUADSPI_SET(CCR, INST, 0x03);	/* simple read */
+	ccr |= QUADSPI_SET(CCR, INST, 0x03);	/* Fast Quad read */
 	ccr |= QUADSPI_SET(CCR, IMODE, QUADSPI_CCR_MODE_1LINE);
 	ccr |= QUADSPI_SET(CCR, ADMODE, QUADSPI_CCR_MODE_1LINE);
 	ccr |= QUADSPI_SET(CCR, ADSIZE, 2);	/* 24 bit address */
@@ -207,12 +212,11 @@ qspi_read_sector(uint32_t addr, uint8_t *buf, int len)
 	printf("After writing CCR status:\n");
 	print_status(QUADSPI_SR);
 	bcnt = 0;
-	while (QUADSPI_SR & QUADSPI_SR_BUSY) {
-		flen = QUADSPI_GET(SR, FLEVEL, QUADSPI_SR);
-		printf("%d-", flen);
-		xfer = (flen >= 4) ? 4 : flen;
+	while ((QUADSPI_SR & QUADSPI_SR_TCF) == 0) {
+		/* flen = QUADSPI_GET(SR, FLEVEL, QUADSPI_SR); */
+/*		printf("%d-", flen);  */
 		tmp = QUADSPI_DR;
-		while (xfer--) {
+		for (i = 0; i < 4; i++) {
 			*buf = tmp & 0xff;
 			bcnt++;
 			tmp >>= 8;
@@ -225,10 +229,162 @@ qspi_read_sector(uint32_t addr, uint8_t *buf, int len)
 	QUADSPI_FCR = QUADSPI_FCR_CTCF;
 }
 
+#define FLASH_SECTOR_ERASE	0xd8
+#define FLASH_WRITE_ENABLE	0x06
+#define FLASH_WRITE_DISABLE	0x04
+#define FLASH_READ_STATUS	0x05
+#define FLASH_WRITE_PAGE	0x02
+
+void qspi_enable(uint8_t cmd);
+void qspi_enable(uint8_t cmd) {
+	uint32_t ccr, sr;
+	
+	switch (cmd) {
+		case FLASH_WRITE_ENABLE:
+			printf("Enable Flash for write.\n");
+			break;
+		case FLASH_WRITE_DISABLE:
+			printf("Disable writing to the flash.\n");
+			break;
+		default:
+			printf("Unrecognized command 0X%x\n", (unsigned int) cmd);
+			return;
+	}
+	ccr = QUADSPI_SET(CCR, INST, cmd);
+	ccr |= QUADSPI_SET(CCR, IMODE, QUADSPI_CCR_MODE_1LINE);
+	ccr |= QUADSPI_SET(CCR, FMODE, QUADSPI_CCR_MODE_IWRITE);
+	QUADSPI_CCR = ccr;
+	do {
+		sr = QUADSPI_SR;
+	} while (sr & QUADSPI_SR_BUSY);
+	if (sr & QUADSPI_SR_TEF) {
+		printf("Done (Error)\n");
+		QUADSPI_FCR = sr; /* reset the flags */
+	} else {
+		printf("Done.\n");
+	}
+}
+
+uint8_t read_flash_status(void);
+/*
+ * Read the Flash chip status register.
+ */
+uint8_t
+read_flash_status(void)
+{
+	uint32_t ccr, sr;
+	uint32_t	res = 0;
+
+	ccr = QUADSPI_SET(CCR, FMODE, QUADSPI_CCR_MODE_IREAD);
+	ccr |= QUADSPI_SET(CCR, INST, FLASH_READ_STATUS);
+	ccr |= QUADSPI_SET(CCR, IMODE, QUADSPI_CCR_MODE_1LINE);
+	ccr |= QUADSPI_SET(CCR, DMODE, QUADSPI_CCR_MODE_1LINE);
+	QUADSPI_DLR = 0; /* one byte to be read */
+	QUADSPI_CCR = ccr;
+	do {
+		sr = QUADSPI_SR;
+		if (QUADSPI_GET(SR, FLEVEL, sr)) {
+			res = QUADSPI_DR;
+		}
+	} while (sr & QUADSPI_SR_BUSY);
+	return (res & 0xff);
+}
+
+
+void qspi_sector_erase(uint32_t addr);
+
+void 
+qspi_sector_erase(uint32_t addr)
+{
+	uint32_t ccr, sr;
+	uint8_t	status;
+
+	printf("Erase Sector: %d\n", (int) addr / 4096);
+	qspi_enable(FLASH_WRITE_ENABLE); /* set the write latch */
+	status = read_flash_status();
+	printf("Status after WRITE ENABLE: 0x%x\n", (unsigned int) status);
+	ccr  = QUADSPI_SET(CCR, FMODE, QUADSPI_CCR_MODE_IWRITE);
+	ccr |= QUADSPI_SET(CCR, ADSIZE, 2);
+	ccr |= QUADSPI_SET(CCR, ADMODE, QUADSPI_CCR_MODE_1LINE);
+	ccr |= QUADSPI_SET(CCR, INST, FLASH_SECTOR_ERASE);
+	ccr |= QUADSPI_SET(CCR, IMODE, QUADSPI_CCR_MODE_1LINE);
+	QUADSPI_CCR = ccr;
+	QUADSPI_AR = addr;
+	do {
+		sr = QUADSPI_SR;
+	} while (sr & QUADSPI_SR_BUSY);
+	status = read_flash_status();
+	printf("Status after SECTOR ERASE: 0x%x\n", (unsigned int) status);
+	do {
+		status = read_flash_status();
+	} while (status & 1); /* write in progress */
+	printf("Done.\n");
+}
+void qspi_write_page(uint32_t addr, uint8_t *buf, int len);
+
+/*
+ * Write a sector.
+ *
+ * The Quad SPI peripheral starts when you write the
+ * address register. 
+ *
+ * From the Data sheet for the flash, fast read needs
+ * 7 dummy cycles at 86Mhz, 8 at 95Mhz. Since we're at
+ * 168Mhz/2 = 84Mhz we're using 7. But if we went to 180/2
+ * we would set it to 8.
+ */
+void
+qspi_write_page(uint32_t addr, uint8_t *buf, int len)
+{
+	uint32_t ccr, sr;
+	uint32_t tmp;
+	int i, status;
+
+	printf("Write Sector\n");
+	qspi_enable(FLASH_WRITE_ENABLE);
+
+	QUADSPI_DLR = len - 1;
+	ccr = QUADSPI_SET(CCR, FMODE, QUADSPI_CCR_MODE_IWRITE);
+	ccr |= QUADSPI_SET(CCR, INST, FLASH_WRITE_PAGE);	/* write 256 bytes */
+	ccr |= QUADSPI_SET(CCR, IMODE, QUADSPI_CCR_MODE_1LINE);
+	ccr |= QUADSPI_SET(CCR, ADMODE, QUADSPI_CCR_MODE_1LINE);
+	ccr |= QUADSPI_SET(CCR, ADSIZE, 2);	/* 24 bit address */
+	ccr |= QUADSPI_SET(CCR, DMODE, QUADSPI_CCR_MODE_1LINE);
+	printf("CCR value = 0x%08X\n", (unsigned int) ccr);
+	printf("CR value = 0x%08X\n", (unsigned int) QUADSPI_CR);
+	QUADSPI_DLR = 255;
+	QUADSPI_CCR = ccr; /* go write a page */
+	QUADSPI_AR = addr;
+	printf("After writing CCR status:\n");
+	print_status(QUADSPI_SR);
+	for (i = 0; i < 256; i+= 4) {
+		tmp = *(buf + i) & 0xff;
+		tmp |= (*(buf + 1 + i) & 0xff) << 8;
+		tmp |= (*(buf + 2 + i) & 0xff) << 16;
+		tmp |= (*(buf + 3 + i) & 0xff) << 24;
+		QUADSPI_DR = tmp;
+	}
+	do {
+		sr = QUADSPI_SR;
+	} while (sr & QUADSPI_SR_BUSY);
+	status = read_flash_status();
+	printf("Status after WRITE PAGE: 0x%x\n", (unsigned int) status);
+	do {
+		status = read_flash_status();
+	} while (status & 1); /* write in progress */
+	printf("Done.\n");
+}
+
+/* one 4K sector from the FLASH chip */
+uint8_t sector_data[4096];
+uint8_t test_sector_data[4096];
+char *test_phrase = "The Quick brown fox jumped over 1,234,567,890 foxes. ";
+
 int
 main(void)
 {
 	uint8_t id_string[80];
+	char *t, *b;
 	int i;
 
 	fprintf(stderr, "QUAD SPI Example/Demo code\n");
@@ -249,13 +405,28 @@ main(void)
 		printf("0x%02X ", id_string[i]);
 	}
 	printf("\n");
-	for (i = 0; i < 256; i++) {
-		sector_data[i] = (uint8_t) i;
+	/* fill the test sector with some data */
+	for (i = 0; i < 10; i++) {
+		t = (char *) &sector_data[i * 53];
+		b = test_phrase;
+		while (*b != 0) {
+			*t++ = *b++;
+		}
 	}
+	printf("Test sector data\n");
 	hex_dump(0x90000000U, sector_data, 256);
-	qspi_read_sector(0, &sector_data[0], 4096);
-	hex_dump(0x90000000U, sector_data, 256);
-
+	qspi_read_sector(0, &test_sector_data[0], 4096);
+	printf("Test sector before writing\n");
+	hex_dump(0x90000000U, test_sector_data, 256);
+	printf("Erasing sector 0\n");
+	qspi_sector_erase(0);
+	printf("Writing test data \n");
+	qspi_write_page(0, &sector_data[0], 256);
+#if 0
+	qspi_read_sector(0, &test_sector_data[0], 4096);
+	printf("Read back test data\n");
+	hex_dump(0x90000000U, test_sector_data, 256);
+#endif
 	while (1) {
 		toggle_led(RED_LED);
 		msleep(500);
