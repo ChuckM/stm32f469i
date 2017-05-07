1fe03ede5436576af08f6aaea50aac85fd1e532f
Chuck McManis <cmcmanis@mcmanis.com>
2017-05-06 18:03:34 -0700

    Updated origin story of sbrk and caveats
    Basically its not different than the library version it
    just uses DRAM instead. Now we don't make that the default
    because we don't always configure the SDRAM chip and so that
    memory isn't always present.

--- demos/util/sbrk.c
+++ demos/util/sbrk.c
@@ -5,6 +5,11 @@
  * in newlib. It uses the DRAM as the heap. The 'frame buffer'
  * for the LCD display is the last 1.5MB and this is 1MB before
  * that.
+ *
+ * It is adapted from the one that is in newlib, it just uses
+ * the DRAM memory instead of __heap_start in the linker script.
+ * that stays the way it is because when we don't have DRAM
+ * configured we don't want to try to use it for dynamic memory!
  */
 #include <stdint.h>
 #include <errno.h>
@@ -17,11 +22,11 @@
 static uint8_t *_cur_brk = (uint8_t *) (FRAMEBUFFER_ADDRESS - MAX_HEAP_SIZE);
 void *_sbrk_r(struct _reent *, ptrdiff_t );
 
-void *_sbrk_r(__attribute__((unused)) struct _reent *reent, ptrdiff_t diff)
+void *_sbrk_r(struct _reent *reent, ptrdiff_t diff)
 {
 	uint8_t *_old_brk = _cur_brk;
     if (_cur_brk + diff > (uint8_t *)(FRAMEBUFFER_ADDRESS)) {
-        errno = ENOMEM;
+        reent->_errno = ENOMEM;
         return (void *)-1;
     }
     _cur_brk += diff;
