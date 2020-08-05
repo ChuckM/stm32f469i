3a554e42d641717d5d0b262dd07eeb692bbdd22d
Chuck McManis <cmcmanis@mcmanis.com>
2020-08-04 18:02:58 -0700

    Intermediate - working interrupts

--- demos/touch/touch.c
+++ demos/touch/touch.c
@@ -16,7 +16,10 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/exti.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/syscfg.h>
 #include <gfx.h>
+
 #include <malloc.h>
 #include "../util/util.h"
 
@@ -27,6 +30,21 @@
 #define FT6206_LIBV_HIGH	0xA1
 #define FT6206_STATE		0xBC
 #define FT6206_THRESH		0x80
+#define FT6206_REPORT		0xA4
+#define FT6206_DELTA_UD		0x95
+#define FT6206_DELTA_LR		0x94
+#define FT6206_DELTA_Z		0x96
+
+typedef enum {
+	GEST_NONE,
+	GEST_UP,
+	GEST_DOWN,
+	GEST_LEFT,
+	GEST_RIGHT,
+	GEST_ZOOM_IN,
+	GEST_ZOOM_OUT,
+	GEST_UNKNOWN
+} touch_gesture;
 
 #define STOP	1
 /*
@@ -53,14 +71,16 @@ print_touch(struct touch_t *t, char *label) {
 i2c_dev *touch_device;
 struct touch_data_t {
 	struct touch_t tp[2];
+	touch_gesture	g;
 	int				n;
 } touch_data[2];
 int touch_ndx = 0;						/* index into touch_data */
 
 /* "working" touch data */
-struct touch_data_t *cur_touch = &touch_data[0];
+struct touch_data_t * volatile cur_touch = &touch_data[0];
 
-volatile int touch_event = 0;
+volatile int touch_detects = 0;
+volatile int gesture_detects = 0;
 
 /*
  * This interrupt it called when the touch device gets a touch
@@ -77,6 +97,7 @@ exti9_5_isr(void)
 {
 	uint8_t buf[16];
 
+	EXTI_PR = (1 << 5);
 	buf[0] = 0;
 	i2c_write(touch_device, buf, 1, 0);
 	i2c_read(touch_device, buf, 16, 1);
@@ -89,26 +110,56 @@ exti9_5_isr(void)
 		cur_touch->tp[i].tid = (buf[ndx+2] & 0xf0) >> 4;
 		cur_touch->tp[i].y = ((buf[ndx+2] & 0xf) << 8) | (buf[ndx+3] & 0xff);
 	}
+	switch(buf[1]) {
+		case 0:
+			cur_touch->g = GEST_NONE;
+			break; 
+		case 0x10:
+			cur_touch->g = GEST_UP;
+			break;
+		case 0x14:
+			cur_touch->g = GEST_RIGHT;
+			break;
+		case 0x18:
+			cur_touch->g = GEST_DOWN;
+			break;
+		case 0x1c:
+			cur_touch->g = GEST_LEFT;
+			break;
+		case 0x48:
+			cur_touch->g = GEST_ZOOM_IN;
+			break;
+		case 0x49:
+			cur_touch->g = GEST_ZOOM_OUT;
+			break;
+		default:
+			cur_touch->g = GEST_UNKNOWN;
+			break;
+	}
 	cur_touch->n = buf[2];
-	touch_event++;
+	if (cur_touch->g != GEST_NONE) {
+		gesture_detects++;
+	}
+	touch_detects++;
 }
 
-struct touch_data_t  *get_touch(void);
+struct touch_data_t  *get_touch(int wait);
 
 /*
  * Check the current touch structure, see if it has seen a touch.
  * If so, swap touch structures and return the current one.
  */
 struct touch_data_t *
-get_touch(void)
+get_touch(int wait)
 {
 	struct touch_data_t *nxt = &touch_data[(touch_ndx + 1) & 1];
 	struct touch_data_t *res = cur_touch;
 
 	/* if n == 0, haven't seen any touching */
-	if (!cur_touch->n) {
+	if ((cur_touch->n == 0) && (wait == 0)) {
 		return NULL;
 	}
+	while (cur_touch->n == 0) ;
 	nxt->n = 0;
 	cur_touch = nxt;
 	return res;
@@ -169,11 +220,40 @@ main(void)
 	int		count;
 	char	*chip_id;
 
+	printf("\033[1;1H\033[J\n");
+	fprintf(stderr, "I2C Example Code\n");
+
 	rcc_periph_clock_enable(RCC_GPIOJ);
 	gpio_mode_setup(GPIOJ, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO5);
 
-	printf("\033[1;1H\033[J\n");
-	fprintf(stderr, "I2C Example Code\n");
+	/* enable interrupts from PJ5 */
+	rcc_periph_clock_enable(RCC_SYSCFG);
+	/* set the source to Port J (0x9), pin 5 */
+	SYSCFG_EXTICR2 = (0x9 << 4);
+
+	printf("SYSCFG_EXTICR2 @0x%0x = %0lx\n", (SYSCFG_BASE + 0xc), 
+						SYSCFG_EXTICR2);
+
+	/* enable interrupts */
+	EXTI_IMR |= (1 << 5);
+	/* interrupt on falling edge */
+	EXTI_RTSR |= (1 << 5);
+	EXTI_FTSR |= (1 << 5);
+	/* Turn on interrupts */
+	nvic_enable_irq(NVIC_EXTI9_5_IRQ);
+
+	if (EXTI_PR & (1 << 5)) {
+		printf("Reset pending interrupt\n");
+		EXTI_PR = (1 << 5);
+	}
+
+	/*
+ 	 * send a test interrupt (this crashes the code?)
+ 	 */
+//	EXTI_SWIER = (1 << 5);
+
+//	printf("\033[1;1H\033[J\n");
+//	fprintf(stderr, "I2C Example Code\n");
 	touch_device = i2c_init(1, I2C_400KHZ, 0x54);
 	if (touch_device == NULL) {
 		fprintf(stderr, "Unable to initialize i2c\n");
@@ -212,45 +292,43 @@ main(void)
 	} else {
 		printf("    State: 0x%02x\n", res);
 	}
+	res = read_reg(touch_device, FT6206_REPORT);
+	printf("     Mode: %s\n", (res == 0) ? "Polling" : "Trigger");
 	/* This effectively kicks it off */
 	write_reg(touch_device, FT6206_THRESH, 0x80);
 	
 	count = 0;
 	while (1) {
-		struct touch_t t[2];
-		int val[15];
+		struct touch_data_t *td;
+#if 0
 		uint8_t buf[16];
+#endif
 
-		while(gpio_get(GPIOJ, GPIO5) != 0) ;
+		td = get_touch(1);
 		count++;
-		printf("\033[8;1H\n");
+		printf("\033[8;1H\033[J\n");
+		printf("\nTotal touch events : %d\n\n", touch_detects);
+		printf("\nTotal gesture events : %d\n\n", gesture_detects);
+#if 0
 		printf("Chip registers:\n");
 		buf[0] = 0;
 		i2c_write(touch_device, buf, 1, 0);
 		i2c_read(touch_device, buf, 16, 1);
-		for (int i = 0; i < 15; i++) {
-			val[i] = read_reg(touch_device, i);
-			if (val[i] < 0) {
-			    printf("  Register %2d : <read error>", i);
-			} else {
-				printf("  Register %2d : 0x%02x (0x%02x)\n", i, 
-						(uint8_t) val[i], buf[i]);
-			}
-		}
-		printf("\nTotal touch events : %d\n\n", count);
 		/* extract touch points */
 		for (int i = 0; i < 2; i++) {
 			int ndx = i*6 + 3;
-			t[i].evt = (val[ndx] & 0xc0) >> 6;
-			t[i].x = ((val[ndx] & 0x3f) << 8) | (val[ndx+1] & 0xff);
-			t[i].tid = (val[ndx+2] & 0xf0) >> 4;
-			t[i].y = ((val[ndx+2] & 0xf) << 8) | (val[ndx+3] & 0xff);
-			t[i].weight = val[ndx+4] & 0xff;
-			t[i].misc = val[ndx+5] & 0xf0 >> 4;
+			t[i].evt = (buf[ndx] & 0xc0) >> 6;
+			t[i].x = ((buf[ndx] & 0x3f) << 8) | (buf[ndx+1] & 0xff);
+			t[i].tid = (buf[ndx+2] & 0xf0) >> 4;
+			t[i].y = ((buf[ndx+2] & 0xf) << 8) | (buf[ndx+3] & 0xff);
+			t[i].weight = buf[ndx+4] & 0xff;
+			t[i].misc = buf[ndx+5] & 0xf0 >> 4;
 		}
-		print_touch(&t[0], "Touch 1");
-		if ((val[2] & 0x3) == 2) {
-			print_touch(&t[1], "Touch 2");
+		printf("\033[J\n");
+#endif
+		print_touch(&(td->tp[0]), "Touch 1");
+		if ((td->n & 0x3) == 2) {
+			print_touch(&(td->tp[1]), "Touch 2");
 		} else {
 			printf("\033[J\n");
 		}
